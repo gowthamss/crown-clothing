3d6026a3c2328baff7745e0c2539b3cdfa40c148
bssgowtham <55312214+bssgowtham@users.noreply.github.com>
2021-06-18 10:33:35 +0530

    implemented code that fetches shop data from the firebase and also the withSpinner hoc

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,19 +2,71 @@ import React from "react";
 import "./shop.styles.scss";
 
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+import {
+    firestore,
+    covertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+import { updateCollections } from "../../redux/shop/shop.action";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
-const ShopPage = ({ match }) => (
-    <div className="shop-page">
-        {/* In the smae ShopPage, we are showing two components with nested routing. When it matches /shop, it renders all collection items. When it matches /shop/:collectionId, it renders the respective collection. */}
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
-        <Route
-            path={`${match.path}/:collectionId`}
-            component={CollectionPage}
-        />
-    </div>
-);
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
-export default ShopPage;
+class ShopPage extends React.Component {
+    state = {
+        loading: true,
+    };
+
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection("collections");
+
+        collectionRef.onSnapshot(async (snapshot) => {
+            const collectionsMap = covertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+            this.setState({ loading: false });
+        });
+    }
+
+    render() {
+        const { match } = this.props;
+        const { loading } = this.state;
+        return (
+            <div className="shop-page">
+                {/* In the smae ShopPage, we are showing two components with nested routing. When it matches /shop, it renders all collection items. When it matches /shop/:collectionId, it renders the respective collection. */}
+                <Route
+                    exact
+                    path={`${match.path}`}
+                    render={(props) => (
+                        <CollectionOverviewWithSpinner
+                            isLoading={loading}
+                            {...props}
+                        />
+                    )}
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    render={(props) => (
+                        <CollectionPageWithSpinner
+                            isLoading={loading}
+                            {...props}
+                        />
+                    )}
+                />
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    updateCollections: (collectionsMap) =>
+        dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
